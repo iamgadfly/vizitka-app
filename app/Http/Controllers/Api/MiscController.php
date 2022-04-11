37bbc973b270a105422fba34cc9681bf41acef2f
Nikolay <nikolay.semenovskiy@softlex.pro>
2022-04-11 12:03:22 +0000

    Feature/review fixes

--- app/Http/Controllers/Api/MiscController.php
+++ app/Http/Controllers/Api/MiscController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\MiscService;
 use GuzzleHttp\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class MiscController extends Controller
@@ -13,10 +14,20 @@ class MiscController extends Controller
         protected MiscService $service
     ){}
 
-    public function getCountries()
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getCountries(): JsonResponse
     {
         return $this->success(
             $this->service->getCountries()
+        );
+    }
+
+    public function getBackgrounds(): JsonResponse
+    {
+        return $this->success(
+            $this->service->getBackgrounds()
         );
     }
 }
