65648fb692c905bc9b3c776d3a8211a3f2a3813f
Semenovskiy <7x8x9x0x>
2022-04-12 13:14:44 +0300

    update TestController

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Helpers\CardBackgroundHelper;
+use App\Helpers\ImageHelper;
 use App\Models\Client;
+use App\Models\Image;
 use App\Models\Specialist;
 use App\Models\User;
 use App\Services\SMSService;
@@ -13,22 +15,6 @@ class TestController extends Controller
 {
     public function test()
     {
-        try {
-            dd(Specialist::all()->toArray());
-            DB::beginTransaction();
-            $user = new User();
-            $user->phone_number = 'c';
-            $user->save();
-            $specialist = new Specialist();
-            $specialist->user_id = $user->id;
-            $specialist->name = 'a';
-            $specialist->surname = 'b';
-            $specialist->activity_kind_id = 0;
-            $specialist->save();
-            DB::commit();
-        } catch (\PDOException $e) {
-            DB::rollBack();
-            throw $e;
-        }
+        return ImageHelper::getAssetFromFilename('images/card_backgrounds/test.svg');
     }
 }
