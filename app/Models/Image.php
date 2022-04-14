eca78e58ef8c54b6efa0a2569e3209afd3fc1564
Semenovskiy <7x8x9x0x>
2022-04-14 15:42:24 +0300

    feature/minor-improvements: make Image model prunable

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -4,12 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 
 class Image extends Model
 {
-    use HasFactory;
+    use HasFactory, Prunable;
 
     protected $guarded = ['id'];
+
+    public function prunable()
+    {
+        return static::query()->where('deleted_at', '<=', now()->subDay());
+    }
+
+    public function pruning()
+    {
+        \Storage::disk('public')->delete($this->url);
+    }
 
     protected $casts = [
         'deleted_at' => 'datetime'
