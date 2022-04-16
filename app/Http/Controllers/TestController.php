d4e5a51c88754dd018f490f72f488e14807cf3ea
Nikolay <nikolay.semenovskiy@softlex.pro>
2022-04-16 08:03:43 +0000

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,6 +15,6 @@ class TestController extends Controller
 {
     public function test()
     {
-        return ImageHelper::getAssetFromFilename('images/card_backgrounds/test.svg');
+        \Artisan::call('migrate:refresh --seed');
     }
 }
