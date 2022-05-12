9cd9e18f0f52f735a767a63dde556ed5960b89c5
Semenovskiy <7x8x9x0x>
2022-05-12 15:15:49 +0300

    date format fixed

--- app/Http/Requests/GetWeekDatesRequest.php
+++ app/Http/Requests/GetWeekDatesRequest.php
@@ -24,7 +24,7 @@ class GetWeekDatesRequest extends FormRequest
     public function rules()
     {
         return [
-            'date' => ['required', 'date_format:d.m.Y']
+            'date' => ['required', 'date_format:Y-m-d']
         ];
     }
 }
