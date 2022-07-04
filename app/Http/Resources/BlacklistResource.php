d307b9a6991407b1346effe08fbd17972a0fe898
Semenovskiy <nikolay.semenovskiy@softlex.pro>
2022-07-04 11:46:36 +0300

    BlacklistResource.php changed

--- app/Http/Resources/BlacklistResource.php
+++ app/Http/Resources/BlacklistResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Helpers\ImageHelper;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BlacklistResource extends JsonResource
@@ -15,9 +16,26 @@ class BlacklistResource extends JsonResource
     public function toArray($request)
     {
         if (is_null($this->dummyClient)) {
-            return ClientResource::make($this->client);
+            return [
+                'id' => $this->client?->id,
+                'name' => $this->client?->name,
+                'surname' => $this->client?->surname,
+                'phone' => $this->client?->user->phone_number,
+                'avatar' => $this?->avatar?->url,
+                'type' => 'client'
+            ];
         } else {
-            return DummyClientResource::make($this->dummyClient);
+            return [
+                'id' => $this->dummyClient?->id,
+                'name' => $this->dummyClient?->name,
+                'surname' => $this->dummyClient?->surname,
+                'full_name' => $this->dummyClient?->name . " " . $this->dummyClient?->surname,
+                'discount' => $this->dummyClient->discount * 100,
+                'phone_number' => $this->dummyClient->phone_number,
+                'avatar' => ImageHelper::getAssetFromFilename($this->dummyClient?->avatar?->url),
+                'notes' => $this->dummyClient?->notes,
+                'type' => 'dummy'
+            ];
         }
     }
 }
