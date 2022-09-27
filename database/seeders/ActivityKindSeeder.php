a041e36c0c1a1accd775f26ad4e33d21e2f2ca3f
Maksim Sorokin <sms@mik4.ru>
2022-09-27 11:53:33 +0000

    Revert "Merge branch '@sms' into 'main'"
    This reverts merge request !4

--- database/seeders/ActivityKindSeeder.php
+++ database/seeders/ActivityKindSeeder.php
@@ -16,66 +16,46 @@ class ActivityKindSeeder extends Seeder
     public function run()
     {
         DB::table('activity_kinds')->insert([
-            [
+           [
                'name' => 'Парикмахер',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'name' => 'Мастер маникюра',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'name' => 'Мастер депиляции',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],           
-            [
+           ],
+           [
                'name' => 'Визажист',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-               'name' => 'Бровист',
-               'created_at' => date('Y-m-d H:i:s'),
-               'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-               'name' => 'Лэшмейкер',
-               'created_at' => date('Y-m-d H:i:s'),
-               'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-               'name' => 'Массажист',
-               'created_at' => date('Y-m-d H:i:s'),
-               'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
+           ],
+           [
                'name' => 'Психолог',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
+           ],
+           [
+               'name' => 'Массажист',
+               'created_at' => date('Y-m-d H:i:s'),
+               'updated_at' => date('Y-m-d H:i:s'),
+           ],
+           [
+               'name' => 'Логопед',
+               'created_at' => date('Y-m-d H:i:s'),
+               'updated_at' => date('Y-m-d H:i:s'),
+           ],
+           [
+               'name' => 'Диетолог',
+               'created_at' => date('Y-m-d H:i:s'),
+               'updated_at' => date('Y-m-d H:i:s'),
+           ],
+           [
                'name' => 'Фитнес-тренер',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-               'name' => 'Фотограф',
+           ],
+           [
+               'name' => 'Другое',
                'created_at' => date('Y-m-d H:i:s'),
                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'name' => 'Репетитор',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ],
-            [
-                'name' => 'Другое',
-                'created_at' => date('Y-m-d H:i:s'),
-                'updated_at' => date('Y-m-d H:i:s'),
-            ]
+           ]
         ]);
     }
 }
